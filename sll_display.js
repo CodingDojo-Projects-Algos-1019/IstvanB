20eabd616f86647868ee9f46de8b0411e51f03e7
Istvan <48108990+Hugo-Stiglitz@users.noreply.github.com>
2019-10-25 10:32:51 -0700

    Add files via upload

---
+++ sll_display.js
@@ -0,0 +1,69 @@
+
+class Node{
+    constructor(value){
+        this.value = value
+        this.next = null;
+    }
+}
+
+class List{
+    constructor(){
+        this.head = null;
+        this.size = 0;
+        
+    }
+    addFront(value){
+        const node = new Node(value);
+
+        node.next = this.head;    //saves the head data to next so it's not destroyed
+        this.head = node;
+    }
+    removeFront(){
+        if (this.head === null){
+            return null;
+        }
+        const node = this.head;
+
+        this.head = node.next;   //saves the head data to next so it's not destroyed
+
+        return node.value;
+    }
+    indexOf(value){       //search list for element
+        var count = 0;
+        var current = this.head;
+
+        while (current != null){
+            if (current.value === value)
+                return count;
+            count++;
+            current = current.next;
+        }
+        return -1;        //This tells if not found
+    }
+    display(){
+        var cur = this.head;
+        var mylist = "";
+
+        while (cur) {
+            mylist += cur.value + " ";
+            cur = cur.next;
+        }
+        console.log(mylist);
+    }
+}
+
+const list = new List();  //construtor based object
+list.addFront('Mike');
+list.addFront('Amy');
+list.addFront('Rudy');
+console.log(list);
+
+console.log(list.removeFront());
+console.log(list);
+
+console.log(list.indexOf('Mike'));
+
+list.display();
+
+
+
